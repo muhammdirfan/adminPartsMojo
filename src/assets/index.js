9e0d668e59e17f152de9eff90f856f92b0fae277
muhammdirfan <mirfan062231@gmail.com>
2023-12-30 00:06:59 +0500

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import BrandImg from "./img/logos/PNG/Logo WHITE H COLOR.png";
+import BrandBlackImg from "./img/logos/PNG/Logo BLACK H.png";
+import BrandColorImg from "./img/logos/PNG/LogoCOLORH.png";
+
+export { BrandImg, BrandBlackImg, BrandColorImg };
